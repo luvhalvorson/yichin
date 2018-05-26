230e52ae1e18cfcf1ae5ae92145a795f405012c7
Jellyfish <turtle20953809@gmail.com>
2018-05-26 17:40:11 +0800

    yichin

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='yichin',
+  packages=['yichin'],
+  version='0.1',
+  description='yichin encode/decode',
+  author='Jellyfish',
+  author_email='turtle20953809@gmail.com',
+  url='https://github.com/killua4564/',
+  download_url='https://github.com/killua4564/yichin',
+  keywords=['yichin', 'encode', 'decode', 'whitespace'],
+  classifiers=[],
+)
