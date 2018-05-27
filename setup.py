c3b6d73821ef6d5f1ccd3ec1241f74d3901dc73a
Jellyfish <turtle20953809@gmail.com>
2018-05-27 11:27:26 +0800

    owo

--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ setup(
   description='yichin encode/decode',
   author='Jellyfish',
   author_email='turtle20953809@gmail.com',
-  url='https://github.com/killua4564/',
-  download_url='https://github.com/killua4564/yichin',
+  url='https://github.com/killua4564/yichin/',
+  download_url='https://github.com/killua4564/yichin/blob/master/dist/yichin-0.1.tar.gz',
   keywords=['yichin', 'encode', 'decode', 'whitespace'],
-  classifiers=[],
+  classifiers=(
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ),
 )
